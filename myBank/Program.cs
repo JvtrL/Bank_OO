94691256337fe8d023ac6efdf3e1968daeaae251
JvtrL <joaovitorlimadasilveira@gmail.com>
2021-09-14 09:49:44 -0300

    Demos continuidade ao desenvolvimento do código.

---
+++ myBank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace myBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaCorrente contaDoJoaoV = new ContaCorrente("JoaoV", 1234, 789, 100.50);
+
+            ContaCorrente contaDoTuke = new ContaCorrente("Tuke", 1234, 790, 50.50);
+
+            ContaCorrente contaDaTiaVirginia = new ContaCorrente("TiaVirginia", 1234, 907, 10000.99);
+
+            Console.WriteLine("O titular da conta é:" + contaDoJoaoV.titular);
+            Console.WriteLine("O titular da conta é:" + contaDoTuke.titular);
+            Console.WriteLine("O titular da conta é:" + contaDaTiaVirginia.titular);
+
+        }
+    }
+}
