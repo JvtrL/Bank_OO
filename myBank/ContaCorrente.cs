c4e2daf2849a9789b21dc670cbd33e9924d6d690
JvtrL <joaovitorlimadasilveira@gmail.com>
2021-09-15 09:37:37 -0300

    Adicionada novas funções.

--- myBank/ContaCorrente.cs
+++ myBank/ContaCorrente.cs
@@ -15,4 +15,36 @@ public class ContaCorrente{
         this.saldo = saldo;
     }
 
+    public bool Sacar(double valor)
+    {
+        if(this.saldo < valor)
+        {
+            return false;
+        }
+        else
+        {
+            this.saldo -= valor;
+            return true;
+        }
+    }
+
+    public void Depositar(double valor)
+    {
+        this.saldo += valor;
+    }
+
+    public bool Transferir(double valor, ContaCorrente contaDestino)
+    {
+        if(this.saldo < valor)
+        {
+            return false;
+        }
+        else
+        {
+            this.saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+    }
+    
 }
--- myBank/Program.cs
+++ myBank/Program.cs
@@ -7,16 +7,32 @@ namespace myBank
         static void Main(string[] args)
         {
 
-            ContaCorrente contaDoJoaoV = new ContaCorrente("JoaoV", 1234, 789, 100.50);
+            ContaCorrente contaDoJoaoV = new ContaCorrente("Joao V", 1234, 789, 100.50);
 
             ContaCorrente contaDoTuke = new ContaCorrente("Tuke", 1234, 790, 50.50);
 
-            ContaCorrente contaDaTiaVirginia = new ContaCorrente("TiaVirginia", 1234, 907, 10000.99);
+            ContaCorrente contaDaTiaVirginia = new ContaCorrente("Tia Virginia", 1234, 907, 10000.99);
 
             Console.WriteLine("O titular da conta é:" + contaDoJoaoV.titular);
             Console.WriteLine("O titular da conta é:" + contaDoTuke.titular);
             Console.WriteLine("O titular da conta é:" + contaDaTiaVirginia.titular);
 
+            contaDoJoaoV.Sacar(90);
+            contaDoTuke.Sacar(0.10);
+            contaDaTiaVirginia.Sacar(700.99);
+
+            Console.WriteLine("O saldo da conta do JoaoV é:" + contaDoJoaoV.saldo);
+            Console.WriteLine("O saldo da conta do Tuke é:" + contaDoTuke.saldo);
+            Console.WriteLine("O saldo da conta da Tia Virginia é:" + contaDaTiaVirginia.saldo);
+
+            contaDoJoaoV.Depositar(1200);
+
+            Console.WriteLine("O saldo da conta do JoaoV é:" + contaDoJoaoV.saldo);
+
+            contaDaTiaVirginia.Transferir(200, contaDoTuke);
+
+            Console.WriteLine("O saldo da conta da Tia Virginia é:" + contaDaTiaVirginia.saldo);
+            Console.WriteLine("O saldo da conta do Tuke é:" + contaDoTuke.saldo);
         }
     }
 }
